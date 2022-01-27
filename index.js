8145e4a048da273bcf66af38fe447be4eba88ed6
Justin Luciano <justinlluciano@gmail.com>
2022-01-27 10:18:24 -0800

    saving

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.getElementById("main").remove();
+const el = document.createElement("h1");
+el.id = "victory";
+el.textContent = "Justin is the champion";
+const newHeader = el;
